fc6243a931198252e0995cb2186af8bbb926d8ce
liaorongsen <wb-lrs192703@alibaba-inc.com>
2017-07-11 15:38:44 +0800

    test_merge_commit_02

--- app/src/main/java/com/lrs/test/activity/main/MainActivity.java
+++ app/src/main/java/com/lrs/test/activity/main/MainActivity.java
@@ -31,7 +31,9 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemClic
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
